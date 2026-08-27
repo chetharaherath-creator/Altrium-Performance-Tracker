2a14a1779a33eff7131b550a2427f9c72994d63e
chetharaherath-creator <chetharaherath@gmail.com>
2026-08-27 17:36:50 +0530

    Configure dynamic API URL for production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // Changed from 5000 to 5001
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
   headers: {
     'Content-Type': 'application/json'
   }
